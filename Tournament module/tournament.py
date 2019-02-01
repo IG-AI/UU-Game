769409e68f20810b4bdf903a55e65a3c4f5100dc
Daniel Ågstrand <Daniel.Agstrand.5971@student.uu.se>
2019-02-01 15:42:02 +0100

    Added other choose_players function that just picks two opponents

--- Tournament module/tournament.py
+++ Tournament module/tournament.py
@@ -5,7 +5,7 @@ class Tournament:
     # To get the opponents do as follows: tournament1 = Tournament(player_list), opponents = tournament1.opponent_list
     def __init__(self, player_list):
         self.player_list = player_list
-        self.opponent_list = choose_players(self.player_list)
+        self.opponents_list = choose_two_players(self.player_list)
 
     # Receives the winners in the form of a list from the client and updates the opponent_list with new opponents.
     # To get new opponents do as follows: tournament1.set_winner(winner_list), opponents = tournament1.opponent_list
@@ -16,11 +16,22 @@ class Tournament:
             print("Player ", self.player_list[0])
             print(' Congratulations!')
             return ()
-        self.opponent_list = choose_players(self.player_list)
+        self.opponents_list = choose_two_players(self.player_list)
 
-# Helps the class to select the opponents and then update the opponent_list in the class.
-def choose_players(player_list):
-    opponent_list = []
+# Helps the class to select two opponents and then update the opponent_list in the class.
+def choose_two_players(player_list):
+    opponents_list = []
+    player1 = random.choice(player_list)
+    player_list.remove(player1)
+    player2 = random.choice(player_list)
+    player_list.remove(player2)
+    opponents = [player1, player2]
+    opponents_list.append(opponents)
+    return opponents_list
+
+# Helps the class to select a list with opponents and then update the opponent_list in the class.
+def choose_players_list(player_list):
+    opponents_list = []
     player_number = len(player_list)
     if player_number % 2 == 0:
         for i in range(int(player_number / 2)):
@@ -28,16 +39,19 @@ def choose_players(player_list):
             player_list.remove(player1)
             player2 = random.choice(player_list)
             player_list.remove(player2)
-            opponent = [player1, player2]
-            opponent_list.append(opponent)
-        return opponent_list
+            opponents = [player1, player2]
+            opponents_list.append(opponents)
+        return opponents_list
     else:
         player1 = random.choice(player_list)
         player_list.remove(player1)
         player2 = random.choice(player_list)
         player_list.remove(player2)
-        opponent = [player1, player2]
-        opponent_list.append(opponent)
-        return opponent_list
+        opponents = [player1, player2]
+        opponents_list.append(opponents)
+        return opponents_list
+
+
+
 
 
