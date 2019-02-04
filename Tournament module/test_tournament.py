31218596180020b20c93baa15b3550c6854095ff
Daniel Ågstrand <Daniel.Agstrand.5971@student.uu.se>
2019-02-04 22:15:16 +0100

    Updated the code to send single opponents instead of a list of opponents

--- Tournament module/test_tournament.py
+++ Tournament module/test_tournament.py
@@ -2,22 +2,19 @@ import tournament as t
 import random
 
 def main():
-    players = 9
-    tournament_depth = 5
+    players = 8
     player_list = []
     for i in range(players):
         player_list.append(i)
     tournament1 = t.Tournament(player_list)
-    print("Chosen opponents: ", tournament1.opponents_list)
-    for i in range((tournament_depth) - 1):
-        winner_list = []
-        for i in range(len(tournament1.opponents_list)):
-            random_number = random.randint(0, 1)
-            winner = tournament1.opponents_list[i][random_number]
-            winner_list.append(winner)
-        print("Winners: ",  winner_list)
-        tournament1.update_opponents(winner_list)
-        print("New opponents: ", tournament1.opponents_list)
+    while tournament1.winner_state == 0:
+        print("Players left: ", tournament1.current_player_list)
+        random_number = random.randint(0, 1)
+        print("Opponents: ", tournament1.opponents)
+        winner = tournament1.opponents[random_number]
+        print("Winners this game: ",  winner)
+        tournament1.get_opponents(winner)
+    print("WINNER: ", tournament1.winner_list[0])
 
 if __name__ == "__main__":
-	main()
+    main()
